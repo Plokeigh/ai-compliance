3c55a6bcb904519026be09d97867c8171af63192
Vincent Washburn <vinceblk1993@gmail.com>
2025-02-19 03:17:16 -0600

    didn't work t2

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['your-image-domain.com'], // Add your image domain here
+  },
+}
+
+module.exports = nextConfig 
